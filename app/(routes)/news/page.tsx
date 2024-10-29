92289c7d0e9f28c373d3758e90f9467f84a41b5f
dorindumitru <dorincosmin.dumitru@yahoo.com>
2024-10-29 09:40:35 +0200

    added suspense

--- app/(routes)/news/page.tsx
+++ app/(routes)/news/page.tsx
@@ -2,13 +2,16 @@ import NavBar from '@/components/shared/NavBar'
 import NewsArticle from '@/components/shared/NewsArticle'
 import ReduxProvider from '@/redux/reduxProvider'
 import React from 'react'
+import { Suspense } from 'react'
 
 const page = () => {
     return (
         <div className={'h-screen'}>
             <ReduxProvider>
-                <NavBar />
-                <NewsArticle />
+                <Suspense>
+                    <NavBar />
+                    <NewsArticle />
+                </Suspense>
             </ReduxProvider>
         </div>
     )
